6d2145112b3a41f990e08f01732c1753d1058838
P1989singh <prmkmrsingh@yahoo.in>
2023-10-21 22:37:45 +0530

    1.0.0

--- .github/workflows/auto-increment-v.sh
+++ .github/workflows/auto-increment-v.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 #get highest tag number
 VERSION=`git describe --abbrev=0 --tags`
 
-#replace . with space so can split into an array
-VERSION_BITS=(${VERSION//./ })
-
 #get number parts and increase last one by 1
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
+VNUM1=$(echo "$VERSION" | cut -d"." -f1)
+VNUM2=$(echo "$VERSION" | cut -d"." -f2)
+VNUM3=$(echo "$VERSION" | cut -d"." -f3)
 VNUM1=`echo $VNUM1 | sed 's/v//'`
 
 # Check for #major or #minor in commit message and increment the relevant version number
@@ -38,7 +35,7 @@ echo "Updating $VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 #only tag if no tag already (would be better if the git describe command above could have a silent option)
 if [ -z "$NEEDS_TAG" ]; then
